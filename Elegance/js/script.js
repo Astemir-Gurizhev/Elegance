014cbbea6d43c0ff499be65e6786281cb3504dbc
Astemir Gurizhev <120544362+Astemir-Gurizhev@users.noreply.github.com>
2023-03-02 21:15:09 +0300

    Delete Elegance directory

--- Elegance/js/script.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('.header__burger').onclick = function() {
-	document.querySelector('.header-left__menu').classList.toggle('header-left__menu-active')
-	document.querySelector('.header__burger').classList.toggle('active')
-   $('body').toggleClass('lock')
-}
